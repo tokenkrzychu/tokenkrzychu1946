b54ea2c9b4c45edf9ea9f82ece7f35b5e010ddda
student <abc@o2.pl>
2016-01-25 12:17:30 +0100

    siema krzychu

---
+++ NetBeansProjects/MikolajKrzychu/src/mikolajkrzychu/MikolajKrzychu.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mikolajkrzychu;
+
+/**
+ *
+ * @author student
+ */
+public class MikolajKrzychu {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+    public int dodawanie(int a, int b)
+    {
+        int wynik = a+b;
+        return wynik;
+    }
+    
+}
